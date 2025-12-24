8f3fc82a47c4485d42fa718d26664ab9177466b4
IRTIDEATH <enderjhass@gmail.com>
2025-12-24 21:19:08 +0700

    setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,47 @@
 import { Module } from '@nestjs/common';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
+import { betterAuth } from 'better-auth';
+import { ConfigModule } from '@nestjs/config';
+import { DATABASE_CONNECTION } from './common/database/database-connection';
+import { NodePgDatabase } from 'drizzle-orm/node-postgres';
+import { DatabaseModule } from './common/database/database.module';
+import { AuthModule } from '@thallesp/nestjs-better-auth';
+import { admin, bearer, openAPI } from 'better-auth/plugins';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    AuthModule.forRootAsync({
+      useFactory: (database: NodePgDatabase) => ({
+        auth: betterAuth({
+          database: drizzleAdapter(database, {
+            provider: 'pg',
+          }),
+          emailAndPassword: {
+            enabled: true,
+            requireEmailVerification: false,
+          },
+          plugins: [openAPI(), admin(), bearer()],
+          user: {
+            additionalFields: {
+              role: {
+                type: 'string',
+                input: false,
+              },
+              premium: {
+                type: 'boolean',
+                input: false,
+              },
+            },
+          },
+          trustedOrigins: [`${process.env.UI_URL}`],
+        }),
+      }),
+      inject: [DATABASE_CONNECTION],
+    }),
+  ],
 })
 export class AppModule {}
