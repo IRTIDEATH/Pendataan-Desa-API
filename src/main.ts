1a4d34a137bffb9aad55ba10340ff5ca05a80488
IRTIDEATH <enderjhass@gmail.com>
2025-12-24 23:11:02 +0700

    fix: mounting better auth

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AuthService } from '@thallesp/nestjs-better-auth';
+import { toNodeHandler } from 'better-auth/node';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -13,6 +16,20 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     preflightContinue: false,
   });
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const expressApp = app.getHttpAdapter().getInstance();
+
+  const authService = app.get<AuthService>(AuthService);
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  expressApp.all(
+    /^\/api\/auth\/.*/,
+    toNodeHandler(authService.instance.handler),
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+  expressApp.use(express.json());
 
   app.setGlobalPrefix('api');
 
