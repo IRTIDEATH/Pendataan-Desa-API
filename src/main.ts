80184b6ee8c3cdc2aac2d97fd810e1ca78893ab0
IRTIDEATH <enderjhass@gmail.com>
2025-12-24 17:32:52 +0700

    init

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    credentials: true,
+    origin: `${process.env.UI_URL}`,
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    preflightContinue: false,
+  });
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(3000);
+}
+bootstrap();
